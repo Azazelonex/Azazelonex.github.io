53c22920f4fcc3daea8bef63e8215e506a39908d
Azazelonex <azazelonex@gmail.com>
2021-09-14 19:56:03 +0300

    new hw3

--- HW3.js
+++ HW3.js
@@ -1,5 +1,13 @@
-console.log((name ??) && (age>18));
-console.log((role===`Admin`) && (occupation ===`CEO`));
+console.log((name ?? `nameEror`) && (age > 18));
+console.log((role === `Admin`) && (occupation === `CEO`));
 console.log(occupation ?? role);
-console.log((name ??) && (age>18) || (role===`Admin`));
-console.log((role!==`Admin`) && (name??) && age);
+console.log((name ?? `nameEror`) && (age > 18) || (role === `Admin`));
+console.log((role !== `Admin`) && (name ?? `nameEror`) && age);
+
+//Если не правильно понял как применя оператор ?? то вот вариант без 
+
+console.log((name !== undefined) && (age > 18));
+console.log((role === `Admin`) && (occupation === `CEO`));
+console.log(occupation ?? role);
+console.log((name !== undefined) && (age > 18) || (role === `Admin`));
+console.log((role !== `Admin`) && (name !== undefined) && age);
