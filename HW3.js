16e2e7427275260e8b561f66ea7653865fa54c03
Azazelonex <85133303+Azazelonex@users.noreply.github.com>
2021-09-14 22:15:30 +0300

    Add files via upload

--- HW3.js
+++ HW3.js
@@ -1,11 +1,3 @@
-console.log((name ?? undefined) && (age > 18));
-console.log((role === `Admin`) && (occupation === `CEO`));
-console.log(occupation ?? role);
-console.log((name ?? undefined) && (age > 18) || (role === `Admin`));
-console.log((role !== `Admin`) && (name ?? undefined) && age);
-
-//Если не правильно понял как применять оператор ?? то вот вариант без 
-
 console.log((name !== undefined) && (age > 18));
 console.log((role === `Admin`) && (occupation === `CEO`));
 console.log(occupation ?? role);
