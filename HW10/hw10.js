12ddcc37f5936be37956dddcb1363c2c77d35c99
Azazelonex <85133303+Azazelonex@users.noreply.github.com>
2021-10-03 23:53:24 +0300

    Add files via upload

--- HW10/hw10.js
+++ HW10/hw10.js
@@ -12,23 +12,23 @@ const obj2 = {name:""};
 isEmpty(obj2);
 console.log(isEmpty(obj2));
 //second
-function showNumbersKey (obj3) {
-    obj3 = {
-      '0': 'zero', 
-      '42': 'answer', 
-      'greeting': 'Hello',
-      '3.14': 'PI', 
-      '543': 'randomNumber',
-      username: "Vasya",
-      '30': 'myAge',
-      secondeName: "Kharmich",
-    } 
-    for (let key in obj3) {
-      if (isNaN(+key)) continue;
-      console.log(obj3[key]);
+function showNumbersKey (obj) {
+  for (let key in obj) {
+    if (isNaN(+key)) continue;
+    console.log(obj[key]);
   }
-  }
-showNumbersKey();
+}
+obj3 = {
+    '0': 'zero', 
+    '42': 'answer', 
+    'greeting': 'Hello',
+    '3.14': 'PI', 
+    '543': 'randomNumber',
+    username: "Vasya",
+    '30': 'myAge',
+    secondeName: "Kharmich",
+} 
+showNumbersKey(obj3);
 //third
 salaries = {
     HRs: { Alice: 500, Bob: 700 },
@@ -36,19 +36,15 @@ salaries = {
     QAs: { Chuck: 1650, Craig: 850, Dave: 3000}
   };
 function doubleSalaries(salaries) {
-    salaries = {
-        HRs: { Alice: 500, Bob: 700 },
-        Devs: { Carol: 2500, Carlos: 1000, Charles: 1500 },
-        QAs: { Chuck: 1650, Craig: 850, Dave: 3000}
-      };
-    let newSalaries = Object.assign({}, salaries);
-    for (let key in newSalaries) {
-    const value = newSalaries[key];
-      for (let i in value) {
-        value[i] *= 2;
-      }
-    } 
-    return newSalaries;
-  }
-  console.log(doubleSalaries());
-  console.log(salaries);
+  let newSalaries = {};
+  for (let key in salaries) {
+    newSalaries[key] = Object.assign({},salaries[key]);
+    for(let userKey in salaries[key]) {
+      newSalaries[key][userKey] = salaries[key][userKey] * 2;
+    }
+  } 
+  return newSalaries;
+}
+
+console.log(doubleSalaries(salaries));
+console.log(salaries);
