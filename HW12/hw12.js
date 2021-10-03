5841db8f80b49a5a92f31b8a99bf52aba7104040
Azazelonex <85133303+Azazelonex@users.noreply.github.com>
2021-10-03 21:44:52 +0300

    Add files via upload

--- HW12/hw12.js
+++ HW12/hw12.js
@@ -15,9 +15,21 @@ function User(firstName ,lastName , age) {
     this.lastName = lastName;
     this.age = age;
     this.greet = function() {
-      alert( `Welcome , i am ${firstName} ${lastName}` );
+      alert( `Welcome , i am ${this.firstName} ${this.lastName}` );
     };
 }
-let student = new User('Alex','Kharmich',30);
+let student = new User('nick','vasya',15);
+student.greet();
+console.log(student);
+
+student = new User('petr','semenich',40);
+student.greet();
+console.log(student);
+
+student = new User('raul','petrivich',60);
+student.greet();
+console.log(student);
+
+student = new User('Alex','andreich',80);
 student.greet();
 console.log(student);
