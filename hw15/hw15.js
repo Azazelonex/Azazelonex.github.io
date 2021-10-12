910394dce79797b56c14fbd57ad9c719ae89447b
Azazelonex <85133303+Azazelonex@users.noreply.github.com>
2021-10-13 00:10:23 +0300

    Add files via upload

---
+++ hw15/hw15.js
@@ -0,0 +1,10 @@
+function keyBy(arr, key) {
+	const newArr = arr.reduce((newArr, user) => {
+		if (!newArr[user[key]]) {
+			newArr[user[key]] = [];
+		}
+		newArr[user[key]].push(user);
+		return newArr;
+	}, {});
+	return newArr;
+}
