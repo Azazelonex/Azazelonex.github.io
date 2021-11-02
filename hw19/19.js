2c5cabe8370dd01c757eda4d6e3a17e3896a5d52
Azazelonex <85133303+Azazelonex@users.noreply.github.com>
2021-11-03 00:16:22 +0200

    Add files via upload

---
+++ hw19/19.js
@@ -0,0 +1,39 @@
+class Menu {
+  constructor(elem) {
+    this._elem = elem;
+    elem.onclick = this.onClick.bind(this);
+  }
+
+  save() {
+    const elemsList = document.querySelector("ol");
+    const newLiElem = document.createElement("li");
+    const inputText = document.querySelector("input");
+
+    newLiElem.innerHTML = `${inputText.value}`;
+    elemsList.append(newLiElem);
+    inputText.value = "";
+  }
+
+  delet() {
+    const markedLiElem = document.querySelector(".marked_li");
+    markedLiElem.remove();
+  }
+
+  onClick(event) {
+    let action = event.target.dataset.action;
+    if (action) {
+      this[action]();
+    }
+  }
+}
+
+new Menu(menu);
+
+const olElem = document.querySelector("ol");
+
+function markLiElem(event) {
+  const liElem = event.target.closest("li");
+  liElem.classList.add("marked_li");
+}
+
+olElem.addEventListener("click", markLiElem);
