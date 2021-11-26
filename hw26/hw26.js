d63ca0317e76ec6ef61bb18aa3816539f8c1e982
Azazelonex <85133303+Azazelonex@users.noreply.github.com>
2021-11-26 18:04:24 +0200

    Add files via upload

--- hw26/hw26.js
+++ hw26/hw26.js
@@ -1,50 +1,64 @@
 class Vegetable {
-	constructor(name) {
-		this.name = name;
-	}
-
+    constructor(name) {
+        this.name = name;
+    }
 }
-
 class Animal {
-	constructor(legs) {
-		this.legs = legs;
-	}
-	eat(food) {
-		this.stomach = [food]
-	}
-	crawl() {
-
-	}
-	walk() {
-
-	}
-	greet() {
-		console.log(`Hi I'm ${firstName + lastName}`)
-	}
+    constructor(legs) {
+        this.legs = legs;
+    }
+    stomach = [];
+    eat(food) {
+        this.stomach.push(food);
+        console.log(`${this.legs} eat food`);
+    }
 }
-
-class Rabbit {
-	constructor(breed) {
-		this.breed = breed;
-	}
-	jump() {
-	}
+class Rabbit extends Animal {
+    constructor(legs, breed) {
+        super(legs);
+        this.breed = breed;
+    }
+    jump() {
+        console.log(`${this.legs} jump`);
+    }
+    eat(food) {
+        if (!(food instanceof Vegetable)) {
+            throw new Error("rabbit can eat only vegetables");
+        }
+        super.eat(food);
+    }
 }
-
-class Snake {
-	constructor(isPoisonous) {
-		this.isPoisonous = isPoisonous
-	}
-
+class Snake extends Animal {
+    constructor(legs, isPoisonous) {
+        super(legs);
+        this.isPoisonous = isPoisonous;
+    }
+    crawl() {
+        console.log(`${this.legs}can crawl`);
+    }
+    eat(food) {
+        if (!(food instanceof Rabbit)) {
+            throw new Error("snake can eat only rabbits");
+        }
+        super.eat(food);
+    }
 }
-
-class Human {
-
-	constructor(firstName, lastName) {
-		this.firstName = firstName
-		this.lastName = lastName
-	}
+class Human extends Animal {
+    constructor(legs, firstName, lastName) {
+        super(legs);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    walk() {
+        console.log(`${this.legs} walk`);
+    }
+    greet() {
+        console.log(`${this.firstName} ${this.lastName} say hi`);
+    }
+    eat(food) {
+        if (food instanceof Human) {
+            throw new Error("human can't eat humans");
+        }
+        super.eat(food);
+    }
 }
-
-const oneUser = new Human("Vasya", "Ivanov");
-const twoUser = new Human("Jenya", "Petrov");
